acd0ce28e3b561040e9e20ce04f6c8daf76ad10f
AizaYosuke <le0_alecrim@yahoo.com.br>
2023-04-05 20:22:37 -0300

    feat: expand the message component to show the messages in the stream

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,14 +1,34 @@
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Messages'),
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messageStream(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: Text('Sem Dados. Vamos conversar?'),
+          );
+        } else {
+          final msgs = snapshot.data;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs!.length,
+            itemBuilder: (ctx, i) {
+              return Text(msgs[i].text);
+            },
+          );
+        }
+      },
     );
   }
 }
